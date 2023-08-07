5db050b4577990ac4a41538e774a441ee1cfcb07
Minseok Kim <comko123@naver.com>
2023-08-08 01:31:39 +0900

    from : 메인 페이지 구현중

---
+++ src/components/home/option/index.tsx
@@ -0,0 +1,52 @@
+import { useState, FC, useId } from "react"
+import { motion } from "framer-motion"
+import "./index.moudule.scss"
+const Option: FC = () => {
+  const [state, setState] = useState(false)
+  const id = useId()
+  return (
+    <>
+      {state ? (
+        <motion.aside className="ablity-open" layoutId={id}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="ablity-close-point"
+            onClick={() => setState((state) => !state)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </motion.aside>
+      ) : (
+        <motion.aside
+          className="ablity-close"
+          layoutId={id}
+          onClick={() => setState((state) => !state)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1}
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+            />
+          </svg>
+        </motion.aside>
+      )}
+    </>
+  )
+}
+
+export default Option
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,26 +1,37 @@
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
+import { motion } from "framer-motion"
 import "./index.moudule.scss"
 const Layout = ({ children }: { children: ReactNode }) => {
+  const [state, setState] = useState(false)
   return (
     <>
       <header className="header">
         <div className="home-link">home</div>
         <div className="search-box">
-          <svg
+          <motion.input
+            className="search-input"
+            placeholder="search for pokemon name"
+            animate={{
+              scaleX: state ? 1 : 0,
+              transformOrigin: "right center"
+            }}
+            transition={{ duration: 0.3 }}
+          />
+          <motion.svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={3}
             stroke="currentColor"
             className="search-icon"
+            onClick={() => setState((state) => !state)}
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
             />
-          </svg>
-          <input className="search-input" />
+          </motion.svg>
         </div>
       </header>
       <section className="layout-section">{children}</section>
